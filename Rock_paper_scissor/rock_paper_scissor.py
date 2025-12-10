2698c42b99bb5be494c3456148cbf2d7d4cfe933
Ncy7 <chaudharynageshwor@gmail.com>
2025-12-10 22:05:59 +0545

    feat: add rock-paper-scissors game with score tracking and play-again loop

--- Rock_paper_scissor/rock_paper_scissor.py
+++ Rock_paper_scissor/rock_paper_scissor.py
@@ -2,9 +2,12 @@
 # Project 2
 import random
 
-choices = ["rock","paper","scissors"]
+choices = ["rock", "paper", "scissors"]
+
 print("Welcome to Rock Paper Scissors!")
-user_choice = input("Enter your choice: ")
-computer_choice = random.choices(choices)
-print(f"Computer choice is {computer_choice}")
-print(f"User choice is {user_choice}")
+user_choice = input("Enter your choice: ").lower().strip()  #.lower() so ROCK or Rock still works  &  .strip() removes accidental spaces
+# computer_choice = random.choices(choices) # It return a list with one or more items (with replacement). So I get ['rock']
+computer_choice = random.choice(choices)  # (singular) -> returns just the string
+
+print(f"Computer choice: {computer_choice}")
+print(f"User choice: {user_choice}")
