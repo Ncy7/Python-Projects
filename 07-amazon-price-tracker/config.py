eb8787149fc489b17d00e5fff982fc992672ccfe
Ncy7 <132090008+Ncy7@users.noreply.github.com>
2025-12-18 22:00:29 +0545

    Update email configuration for sender and receiver

--- 07-amazon-price-tracker/config.py
+++ 07-amazon-price-tracker/config.py
@@ -1,6 +1,6 @@
 # Email configuration - KEEP THIS PRIVATE!
-EMAIL_SENDER = "chaudharynageshwor@gmail.com"       # Your Gmail address
+EMAIL_SENDER = "sender@gmail.com"       # Your Gmail address
 EMAIL_PASSWORD = "12345678901234567890"        # Gmail App Password (NOT your regular password)
-EMAIL_RECEIVER = "chaudharynageshwor@gmail.com"   # Where to send alerts (can be same as sender)
+EMAIL_RECEIVER = "receiver@gmail.com"   # Where to send alerts (can be same as sender)
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
